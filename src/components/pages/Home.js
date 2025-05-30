a2c2656733bb6b0d0023ad6a08c6d26791250d3e
GabrielPivoto <gabriel-pivoto@outlook.com>
2025-05-30 15:21:37 -0300

    Create Home page
     - Creates Home page
    
    Author: Pivoto

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,16 @@
+import styles from './Home.module.css'
+
+import savings from '../../images/savings.svg'
+import LinkButton from '../layouts/LinkButton'
+
 function Home(){
     return (
-        <h1>Home</h1>
+        <section className={styles.home_container}>
+            <h1>Welcome to <span>Costs</span></h1>
+            <p>Start managing your projects right now!</p>
+            <LinkButton to="/newProject" text="Create new project"/>
+            <img src={savings} alt='Costs'/>
+        </section>
     )
 }
 
