611ddf667af8b5e8a8e560ba0cc05f3dd428f6af
GabrielPivoto <gabriel-pivoto@outlook.com>
2025-05-30 14:27:51 -0300

    Create Navbar and Footer
     - Creates Navbar and Footer
    
    Author: Pivoto

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Home from './components/pages/Home';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contatc';
+import NewProject from './components/pages/NewProject';
+import Projects from './components/pages/Projects';
+
+import Container from './components/layouts/Container';
+import Navbar from './components/layouts/Navbar';
+import Footer from './components/layouts/Footer';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Navbar/>
+      <Container customClass="min-height">
+        <Routes>
+          <Route exact path='/' element={<Home/>}></Route>
+          <Route path='/projects' element={<Projects/>}></Route>
+          <Route path='/company' element={<Company/>}></Route>
+          <Route path='/contact' element={<Contact/>}></Route>
+          <Route path='/newProject' element={<NewProject/>}></Route>
+        </Routes>
+      </Container>
+      <Footer/>
+    </Router>
   );
 }
 
