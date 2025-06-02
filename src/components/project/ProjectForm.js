16800418fbf67943df164a33b8e78b04e61a8559
GabrielPivoto <gabriel-pivoto@outlook.com>
2025-06-02 09:52:39 -0300

    Mock backend api
     - Creates db.json to mock back-end application
     - Fetches category information to display in option component
    
    Author: Pivoto

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,9 +1,26 @@
+import { useState, useEffect } from 'react'
+
 import Input from '../form/Input'
 import Select from '../form/Select'
 import SubmitButton from '../form/SubmitButton'
 import styles from './ProjectForm.module.css'
 
 function ProjectForm({ btnText }) {
+
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:5000/categories', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'aplication/json',
+            },
+        })
+            .then((resp) => resp.json())
+            .then((data) => { setCategories(data) })
+            .catch((err) => { console.log(err) })
+    }, [])
+
     return (
         <form className={styles.project_form}>
 
@@ -17,7 +34,10 @@ function ProjectForm({ btnText }) {
                 type="number"
                 name="budget"
                 placeholder="Insert the project budget" />
-            <Select name="category_id" text="Select a category" />
+            <Select
+                name="category_id"
+                text="Select a category"
+                options={categories} />
             <SubmitButton text={btnText} />
         </form>
     )
