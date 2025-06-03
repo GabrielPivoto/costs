24d9e1a13dd4c3b9aa8bf48f020edaf79ee1b8a0
GabrielPivoto <gabriel-pivoto@outlook.com>
2025-06-03 09:56:06 -0300

    Save project feature
     - Implements feature to save a new project in db.json
    
    Author: Pivoto

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -5,9 +5,10 @@ import Select from '../form/Select'
 import SubmitButton from '../form/SubmitButton'
 import styles from './ProjectForm.module.css'
 
-function ProjectForm({ btnText }) {
+function ProjectForm({ btnText, handleSubmit, projectData }) {
 
     const [categories, setCategories] = useState([])
+    const [project, setProject] = useState(projectData || {})
 
     useEffect(() => {
         fetch('http://localhost:5000/categories', {
@@ -21,23 +22,46 @@ function ProjectForm({ btnText }) {
             .catch((err) => { console.log(err) })
     }, [])
 
-    return (
-        <form className={styles.project_form}>
+    const submit = (e) => {
+        e.preventDefault()
+        handleSubmit(project)
+    }
 
+    function handleChange(e) {
+        setProject({ ...project, [e.target.name]: e.target.value })
+    }
+
+    function handleCategory(e) {
+        setProject({
+            ...project, category: {
+                id: e.target.value,
+                name: e.target.options[e.target.selectedIndex].text,
+            }
+        })
+    }
+
+    return (
+        <form className={styles.project_form} onSubmit={submit}>
             <Input
                 type="text"
                 text="Project Name"
                 name="name"
-                placeholder="Insert the project name" />
+                placeholder="Insert the project name"
+                handleOnChange={handleChange}
+                value={project.name ? project.name : ''} />
             <Input
                 text="Project Budget"
                 type="number"
                 name="budget"
-                placeholder="Insert the project budget" />
+                placeholder="Insert the project budget"
+                handleOnChange={handleChange}
+                value={project.budget ? project.budget : ''} />
             <Select
                 name="category_id"
                 text="Select a category"
-                options={categories} />
+                options={categories}
+                handleOnChange={handleCategory}
+                value={project.category ? project.category.id : ''} />
             <SubmitButton text={btnText} />
         </form>
     )
