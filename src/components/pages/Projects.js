177c72d0a4a1ffea6a7d9b2e26ee1638dcbfacbf
GabrielPivoto <gabriel-pivoto@outlook.com>
2025-06-06 09:56:54 -0300

    Finish Projects Page
     - Finishes projects page displaying all projects from db
    
    Author: Pivoto

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,4 +1,5 @@
 import { useLocation } from "react-router-dom"
+import { useState, useEffect } from 'react'
 
 import styles from './Projects.module.css'
 
@@ -6,25 +7,48 @@ import Message from "../layouts/Message"
 import LinkButton from '../layouts/LinkButton'
 import Container from '../layouts/Container'
 
+import ProjectCard from "../project/ProjectCard"
+
 function Projects() {
 
     const location = useLocation()
+    const [projects, setProjects] = useState([])
     let message = ''
+
     if (location.state) {
         message = location.state.message
     }
+
+    useEffect(() => {
+        fetch('http://localhost:5000/projects', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'aplication/json',
+            },
+        })
+            .then((resp) => resp.json())
+            .then((data) => { setProjects(data) })
+            .catch((err) => { console.log(err) })
+    }, [])
 
     return (
         <div className={styles.project_container}>
             <div className={styles.title_container}>
                 <h1>Projects</h1>
-                <LinkButton to="/newProject" text="New Project"/>
+                <LinkButton to="/newProject" text="New Project" />
             </div>
             {message && (
                 <Message type="success" message={message} />
             )}
             <Container customClass="start">
-                <p>Projects...</p>
+                {projects.length > 0 && projects.map((project) => (
+                    <ProjectCard
+                        id={project.id}
+                        projectName={project.name}
+                        budget={project.budget}
+                        category={project.category.name}
+                        key={project.id} />
+                ))}
             </Container>
         </div>
     )
---
+++ src/components/project/ProjectCard.js
@@ -0,0 +1,29 @@
+import { BsPencil, BsFillTrashFill } from 'react-icons/bs'
+import { Link } from 'react-router-dom'
+
+import styles from './ProjectCard.module.css'
+
+function ProjectCard({ id, projectName, budget, category, handleRemove }) {
+
+    return (
+        <div className={styles.project_card}>
+            <h4>{projectName}</h4>
+            <p>
+                <span>Budget:</span> ${budget}
+            </p>
+            <p className={styles.category_text}>
+                <span className={`${styles[category.toLowerCase()]}`}></span> {category}
+            </p>
+            <div className={styles.project_card_actions}>
+                <Link to="/">
+                    <BsPencil/> Edit
+                </Link>
+                <button>
+                    <BsFillTrashFill/> Delete
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectCard
